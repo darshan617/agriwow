758e67d68cfa945849b415fb9d31c65893597d1c
darshan617 <darshan@goyalinfotech.com>
2026-08-26 12:57:20 +0530

    done with addding saving amount, breadcrum changes and design change in product details page

--- src/components/product-details/components/breadcrumb/Breadcrumb.jsx
+++ src/components/product-details/components/breadcrumb/Breadcrumb.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import styles from "@/components/product-category/components/ProductCategoryList/ProductCategoryList.module.css";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 
 const Breadcrumb = ({ productDetails, blogDetails }) => {
+  console.log(productDetails, "ss");
+
+  const categories = useSelector((state) => state.category.categories);
   return (
     <div className="container">
       <div className={`${styles.breadcrumb} `}>
         <div style={{ margin: "16px 0" }}>
           <ul>
             <li>
-              <Link href="/" prefetch={true}>Home</Link>
+              <Link href="/" prefetch={true}>
+                Home
+              </Link>
             </li>
             <li style={{ margin: "0 8px", color: "#6c757d" }}>/</li>
             <li>
-              <Link href="/" prefetch={true}>Products</Link>
+              <Link
+                href={`/product-category/${categories?.[0]?.slug}`}
+                prefetch={true}
+              >
+                Products
+              </Link>
             </li>
             <li style={{ margin: "0 8px", color: "#6c757d" }}>/</li>
             <li className={`${styles.breadcrumbItem1}`}>
@@ -22,6 +33,15 @@ const Breadcrumb = ({ productDetails, blogDetails }) => {
                 prefetch={true}
               >
                 {productDetails?.data?.category?.name}
+              </Link>
+            </li>
+            <li style={{ margin: "0 8px", color: "#6c757d" }}>/</li>
+            <li className={`${styles.breadcrumbItem1}`}>
+              <Link
+                href={`/product-details/${productDetails?.data?.slug}`}
+                prefetch={true}
+              >
+                {productDetails?.data?.name}
               </Link>
             </li>
           </ul>
